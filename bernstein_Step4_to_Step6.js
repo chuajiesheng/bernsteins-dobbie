808fb7807478c37dd1d33202428a2254e57cc7c6
Steve Ng <steveng.1988@gmail.com>
2013-09-25 23:32:49 +0800

    Added psudocode for step 4-6, need to know input eh haha

---
+++ bernstein_Step4_to_Step6.js
@@ -0,0 +1,15 @@
+
+//Step 4 function
+function step4($wtf_is_coming_in){
+
+}
+
+//Step 5 function
+function step5($wtf_is_coming_in){
+
+}
+
+//Step 6 function
+function step6(wtf_is_coming_in){
+
+}
