6de02135c45b23604a878a3c78e44a5ef3082aae
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-09-26 03:14:50 +0800

    Added Mvd to objects prototype

--- objects.js
+++ objects.js
@@ -15,3 +15,22 @@ var Fd = function (lhs, rhs) {
     }
 }
 Fd.prototype = new Fd();
+
+// TODO: support independent of '|'
+var Mvd = function (lhs, rhs) {
+    this.lhs = lhs;
+    this.rhs = rhs;
+    this.str = function () {
+        var fd = '';
+
+        for (i = 0; i < lhs.length; i++) {
+            fd += lhs[i] + ' ';
+        }
+        fd += '&rarr; '
+        for (i = 0; i < rhs.length; i++) {
+            fd += rhs[i] + ' ';
+        }
+        return fd;
+    }
+}
+Mvd.prototype = new Mvd();
