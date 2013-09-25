40d90dca6e923cebd5130b99356df4f48d241a64
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-09-26 01:45:03 +0800

    Implemented fd selected mututally exclusive algorithm

--- fd.js
+++ fd.js
@@ -1,15 +1,48 @@
 $('#fdLHS').on('change', function() {
     var selected_option = $('#fdLHS option:selected');
+    var rhs = $('#fdRHS > option');
 
-    for (i = 0; i < selected_option.length; i++) {
-        $('#fdRHS > option').each(function() {
+    $('#fdRHS > option').each(function() {
+        var isSelected = false;
+        for (i = 0; i < selected_option.length; i++) {
             var matched = selected_option[i].value == this.value;
             if (matched) {
-                $(this).prop('disabled', true);
+                isSelected = true;
             }
-        });
-    }
+        }
+
+        if (isSelected) {
+            $(this).prop('selected', false);
+            $(this).prop('disabled', true);
+        } else {
+            $(this).prop('disabled', false);
+        }
+    });
 });
+
+$('#fdRHS').on('change', function() {
+    var selected_option = $('#fdRHS option:selected');
+    var lhs = $('#fdLHS > option');
+
+    $('#fdLHS > option').each(function() {
+        var isSelected = false;
+        for (i = 0; i < selected_option.length; i++) {
+            var matched = selected_option[i].value == this.value;
+            if (matched) {
+                isSelected = true;
+            }
+        }
+
+        if (isSelected) {
+            $(this).prop('selected', false);
+            $(this).prop('disabled', true);
+        } else {
+            $(this).prop('disabled', false);
+        }
+    });
+});
+
+var fds = [];
 
 $('#addFd').on('click', function () {
     var lhs = $('#fdLHS option:selected');
