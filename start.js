9f3b2e579b4814ad463b9c8306c89fa54f125e11
gp-c <guipei.c@gmail.com>
2013-10-19 01:08:31 +0800

    edited step 1 & 2 in start.js

--- start.js
+++ start.js
@@ -4,34 +4,12 @@ $('#startBernstein').on('click', function () {
     // start bernstein algorithm here
     // step 1
     // pass in fds, return fds
-function step1(fds) {
-    for (var i=0;i<fds.length;i++)
-    {
-        //get closure for attributes on LHS
-        //then exclude LHS attributes from the closure
-        var lhsClosure = closure(fds[i].lhs, fds);
-        lhsClosure = _.difference(lhsClosure, fds[i].lhs);
-      
-        for (var j=0;j<fds.length;j++)
-        {
-            //remove redundant attributes from LHS
-             if(contains(fds[i].lhs, fds[j].lhs)){
-                fds[j].lhs = _.difference(fds[j].lhs, lhsClosure);
-             }
-        }
-    }
-	return fds;
-}
+    fds = step1(fds);
     
     // step 2
     // pass in fds, return fds
-function step2(fds) {
-    for (var i=0;i<fds.length;i++)
-    {
-        fds = covering(fds[i].lhs, fds);    
-    }
-	return fds;
-}
+    fds = step2(fds);
+
     // step 3
 
     // step 4
