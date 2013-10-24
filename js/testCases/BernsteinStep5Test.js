cc1030c03b574dc4487ce896e2593f8507445e60
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-24 18:03:46 +0800

    Fix issue with test case

--- js/testCases/BernsteinStep5Test.js
+++ js/testCases/BernsteinStep5Test.js
@@ -15,10 +15,10 @@ describe("Testing step 5 of Bernstein algo", function() {
     window.groupfds = new Array();
 
     Bernstein.step3(fds);
-    var step3groupfds = window.groupfds; 
+    var step3groupfds = window.groupfds;
 
     Bernstein.step4(step3groupfds);
-    var step4groupfds = window.groupfds; 
+    var step4groupfds = window.groupfds;
 
     Bernstein.step5(step4groupfds);
     var step5groupfds = window.groupfds;
@@ -28,15 +28,15 @@ describe("Testing step 5 of Bernstein algo", function() {
     expectedGroupfds[0] = new Array();
     expectedGroupfds[0][0] = new Fd(["X1","X2"],["C","D"]);
     expectedGroupfds[0][1] = new Fd(["C","D"],["X1","X2"]);
-    expectedGroupfds[1] = new Array(); 
+    expectedGroupfds[1] = new Array();
     //expectedGroupfds[1][0] = new Fd(["X1","X2"],["A"]);
-    expectedGroupfds[2] = new Array(); 
+    expectedGroupfds[2] = new Array();
     expectedGroupfds[2][0] = new Fd(["A","X1"],["B"]);
-    expectedGroupfds[3] = new Array(); 
-    expectedGroupfds[3][0] = new Fd(["B","X2"],["C"]);  
-    expectedGroupfds[4] = new Array(); 
+    expectedGroupfds[3] = new Array();
+    expectedGroupfds[3][0] = new Fd(["B","X2"],["C"]);
+    expectedGroupfds[4] = new Array();
     expectedGroupfds[4][0] = new Fd(["C"],["A"]);
-    //end of expected output 
+    //end of expected output
 
     var step5fds = Bernstein.convertGroupToFds(step5groupfds);
     var expectedfds = Bernstein.convertGroupToFds(expectedGroupfds);
@@ -47,22 +47,22 @@ describe("Testing step 5 of Bernstein algo", function() {
   })
 
   it("Given: A->B and B->C and A->C "+
-      " check removal of A->C as it is a transitive dependencies" 
+      " check removal of A->C as it is a transitive dependencies"
       ,function(){
 
     var fds = new Array();
-    fds[0] = new Fd(["A","B"]);
-    fds[1] = new Fd(["A","C"]);
+    fds[0] = new Fd(["A"],["B"]);
+    fds[1] = new Fd(["A"],["C"]);
     fds[2] = new Fd(["B"],["C"]);
 
     window.fds = fds;
     window.groupfds = new Array();
 
     Bernstein.step3(fds);
-    var step3groupfds = window.groupfds; 
+    var step3groupfds = window.groupfds;
 
     Bernstein.step4(step3groupfds);
-    var step4groupfds = window.groupfds; 
+    var step4groupfds = window.groupfds;
 
     Bernstein.step5(step4groupfds);
     var step5groupfds = window.groupfds;
@@ -71,10 +71,10 @@ describe("Testing step 5 of Bernstein algo", function() {
     var expectedGroupfds = new Array();
     expectedGroupfds[0] = new Array();
     expectedGroupfds[0][0] = new Fd(["A"],["B"]);
-    expectedGroupfds[1] = new Array(); 
+    expectedGroupfds[1] = new Array();
     expectedGroupfds[1][0] = new Fd(["B"],["C"]);
-    
-    //end of expected output 
+
+    //end of expected output
     var step5fds = Bernstein.convertGroupToFds(step5groupfds);
     var expectedfds = Bernstein.convertGroupToFds(expectedGroupfds);
 
